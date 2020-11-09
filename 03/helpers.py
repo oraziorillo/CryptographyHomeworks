5a5375e377d766b4d2d622773941351946b20cf3
oraziorillo <oraziorillo@gmail.com>
2020-11-09 02:02:19 +0100

    SOLVED 2, 3a, Tentative solution of 3b

--- 03/helpers.py
+++ 03/helpers.py
@@ -1,12 +1,12 @@
-def get_input_init_str(filename, ex_number):
+def get_input_init_str(filename, ex):
     f = open(filename, "r")
     next_line = f.readline()
-    while (next_line != "###### Exercise " + str(ex_number) + "\n"):
+    while (next_line != "###### Exercise " + str(ex) + "\n"):
         next_line = f.readline()
 
     input_str = ""
     next_line = f.readline()
-    while (next_line != "###### Exercise " + str(ex_number + 1) + "\n" and next_line != ''):
+    while (next_line != "###### Exercise " + str(ex + 1) + "\n" and next_line != ''):
         input_str += next_line
         next_line = f.readline()
         
